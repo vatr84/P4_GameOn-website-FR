6d7086bd03bce47200d60d5a141038fdda80b555
vatr84 <vatr@hotmail.fr>
2024-06-25 10:22:39 +0200

    js: fermer la modale

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,7 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtnClose = document.querySelector(".close");
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
@@ -20,4 +21,8 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal form 
+modalBtnClose.addEventListener("click", function() {          
+  modalbg.style.display = "none";                             
+});
 
